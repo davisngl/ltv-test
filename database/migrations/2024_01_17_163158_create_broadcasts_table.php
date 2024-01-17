57016d6ee7fc5200485198de43d4f72847a55d0b
Dāvis Naglis <davisnaglis2@gmail.com>
2024-01-17 18:40:59 +0200

    Create base models, their migrations and factories, fill the schema for tables

---
+++ database/migrations/2024_01_17_163158_create_broadcasts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Broadcast;
+use App\Models\Channel;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 100)->unique();
+        });
+
+        Schema::create('broadcast_channel', function (Blueprint $table) {
+            $table->foreignIdFor(Broadcast::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->foreignIdFor(Channel::class)
+                ->constrained()
+                ->cascadeOnDelete();
+            $table->timestamp('starts_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+        });
+    }
+};
