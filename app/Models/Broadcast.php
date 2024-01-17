86adc5b0447f4dc43b6062833bea5e7d85298a40
Dāvis Naglis <davisnaglis2@gmail.com>
2024-01-17 20:11:24 +0200

    Add composer script for IDE helper, generate metadata for models

--- app/Models/Broadcast.php
+++ app/Models/Broadcast.php
@@ -6,6 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Models\Broadcast
+ *
+ * @property int $id
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Channel> $channels
+ * @property-read int|null $channels_count
+ * @method static \Database\Factories\BroadcastFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Broadcast newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Broadcast newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Broadcast query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Broadcast whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Broadcast whereName($value)
+ * @mixin \Eloquent
+ */
 class Broadcast extends Model
 {
     use HasFactory;
--- app/Models/Channel.php
+++ app/Models/Channel.php
@@ -6,6 +6,23 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
+/**
+ * App\Models\Channel
+ *
+ * @property int $id
+ * @property int $number
+ * @property string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Broadcast> $broadcasts
+ * @property-read int|null $broadcasts_count
+ * @method static \Database\Factories\ChannelFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Channel whereNumber($value)
+ * @mixin \Eloquent
+ */
 class Channel extends Model
 {
     use HasFactory;
