a82e1f7090913d3b3d725560705b2a1cd15984dc
Dāvis Naglis <davisnaglis2@gmail.com>
2024-01-18 16:12:19 +0200

    Create general database seeder with dummy broadcasts and initial API token from Sanctum

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,21 +2,35 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Broadcast;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
+    use WithoutModelEvents;
+
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+         $user = User::factory()->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         $dummyBroadcasts = File::json(database_path('seeders/guide/broadcasts.json'));
+
+         collect($dummyBroadcasts)->each(fn (array $broadcast) =>
+            Broadcast::firstOrCreate(['name' => $broadcast['name']])
+         );
+         
+         $this->command->info(
+             vsprintf('%d broadcasts have been seeded for initial usage.', [count($dummyBroadcasts)])
+         );
+
+        $token = $user->createToken('api')->plainTextToken;
+
+        $this->command->info("API token created: $token");
     }
 }
